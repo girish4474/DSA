8713e0f7ab223c98366ef2f203b49259a2b7a009
Venkata Sai Girish <mvsgirish447474@gmail.com>
2024-08-21 22:02:55 +0530

    Add DoublyLinkedList

---
+++ src/datastructures/doublylinkedlist/Main.java
@@ -0,0 +1,34 @@
+package datastructures.doublylinkedlist;
+
+public class Main {
+    public static void main(String a[]) {
+        DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(5);
+        // append and remove Last
+//        myDoublyLinkedList.append(10);
+//        myDoublyLinkedList.append(15);
+//        myDoublyLinkedList.printAll();
+//        DoublyLinkedList.Node removed = myDoublyLinkedList.removeLast();
+//        myDoublyLinkedList.printAll();
+
+        // prepend and remove First
+//        myDoublyLinkedList.prepend(10);
+//        myDoublyLinkedList.prepend(15);
+//        myDoublyLinkedList.printAll();
+//        System.out.println("Removed First: "+myDoublyLinkedList.removeFirst().value);
+//        myDoublyLinkedList.printAll();
+
+        // get and set
+//        myDoublyLinkedList.append(10);
+//        myDoublyLinkedList.append(15);
+//        myDoublyLinkedList.append(20);
+//
+//        System.out.println(myDoublyLinkedList.get(2).value);
+//        System.out.println(myDoublyLinkedList.get(1).value);
+//
+//        System.out.println(myDoublyLinkedList.set(1, 25));
+//        System.out.println(myDoublyLinkedList.set(-2, 30));
+//        myDoublyLinkedList.printAll();
+
+
+    }
+}

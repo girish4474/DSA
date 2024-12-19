99d2ce203dff99ae1fc0474fc119bfedf954befc
Venkata Sai Girish <mvsgirish447474@gmail.com>
2024-12-19 21:51:51 +0530

    Add recursive methods to BST

--- src/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/datastructures/binarysearchtree/BinarySearchTree.java
@@ -13,6 +13,10 @@ public class BinarySearchTree {
         }
     }
 
+    public Node getRoot() {
+        return root;
+    }
+
     /**
      * Insert a new node in the binary search tree
      *
@@ -65,4 +69,82 @@ public class BinarySearchTree {
         }
         return false;
     }
+
+    private boolean rContains(Node currentNode, int value) {
+        if(currentNode == null) return false;
+        if(currentNode.value == value) return true;
+        if(value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        } else {
+            return rContains(currentNode.right, value);
+        }
+    }
+
+    public boolean rContains(int value){
+        return rContains(root, value);
+    }
+
+
+    private Node rInsert(Node currentNode, int value) {
+        if(currentNode == null) return new Node(value);
+        if(value < currentNode.value) {
+            currentNode.left = rInsert(currentNode.left, value);
+        } else if(value > currentNode.value) {
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+        return currentNode;
+    }
+
+    public void rInsert(int value) {
+        if (root == null) root = new Node(value);
+        rInsert(root, value);
+    }
+
+    public int minValue(Node currentNode) {
+        while (currentNode.left != null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+
+        //
+//        if(currentNode.left == null) return currentNode.value;
+//        return minValue(currentNode.left);
+    }
+
+    private Node deleteNode(Node currentNode, int value) {
+        // Base case, tree is empty
+        if (currentNode == null) return null;
+
+        // Recurse down the tree
+        if (value < currentNode.value) {
+            // If less, go left
+            currentNode.left = deleteNode(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            // If more, go right
+            currentNode.right = deleteNode(currentNode.right, value);
+        } else {
+            // Value is same as current's value, node to delete
+            if (currentNode.left == null && currentNode.right == null) {
+                // Node is a leaf node
+                return null;
+            } else if (currentNode.left == null) {
+                // Node has only right child
+                currentNode = currentNode.right;
+            } else if (currentNode.right == null) {
+                // Node has only left child
+                currentNode = currentNode.left;
+            } else {
+                // Node has two children
+                int subTreeMin = minValue(currentNode.right);
+                // Replace with min in right subtree
+                currentNode.value = subTreeMin;
+                // Delete the minimum in right subtree
+                currentNode.right = deleteNode(currentNode.right, subTreeMin);
+            }
+        }
+        // Return the modified tree
+        return currentNode;
+    }
+
+    public void deleteNode(int value) { root = deleteNode(root, value); }
 }
--- src/datastructures/binarysearchtree/Main.java
+++ src/datastructures/binarysearchtree/Main.java
@@ -14,7 +14,7 @@ public class Main{
         myBst.insert(82);
 
 
-        System.out.println("value 27 insertion is: "+myBst.insert(27));
+//        System.out.println("value 27 insertion is: "+myBst.insert(27));
 //        System.out.println("root="+ myBst.root.value);
 //        System.out.println("root.left="+ myBst.root.left.value);
 //        System.out.println("root.right="+ myBst.root.right.value);
@@ -25,9 +25,74 @@ public class Main{
 //        System.out.println("root.right.right="+ myBst.root.right.right.value);
         //--------------------------------------------------------------------------------
 
-        // Check if the tree contains a value
-        System.out.println("BST Contains 27: " + myBst.contains(27)); // true
-        System.out.println("BST Contains 53: " + myBst.contains(53)); // true
+//        // Check if the tree contains a value
+//        System.out.println("BST Contains 27: " + myBst.contains(27)); // true
+//        System.out.println("BST Contains 53: " + myBst.contains(53)); // false
+
+//        System.out.println("BST Contains 21: " + myBst.rContains(21)); // true
+//        System.out.println("BST Contains 17: " + myBst.rContains(17)); // false
+
+
+        //--------------------------------------------------------------------------------
+        // Test cases for recursive insert
+//        BinarySearchTree myBST = new BinarySearchTree();
+//        myBST.rInsert(2);
+//        myBST.rInsert(1);
+//        myBST.rInsert(3);
+//
+//        System.out.println("Root: " + myBST.getRoot().value);
+//        System.out.println("\nRoot->Left: " + myBST.getRoot().left.value);
+//        System.out.println("\nRoot->Right: " + myBST.getRoot().right.value);
+
+//        System.out.println(myBst.minValue( myBst.getRoot() ));
+//
+//        System.out.println(myBst.minValue( myBst.getRoot().right ));
+
+
+
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(2);
+        myBST.insert(1);
+        myBST.insert(3);
+
+        /*
+               2
+              / \
+             1   3
+        */
+
+        System.out.println( "Root: " + myBST.getRoot().value );
+        System.out.println( "Root->Left: " + myBST.getRoot().left.value );
+        System.out.println( "Root->Right: " + myBST.getRoot().right.value );
+
+
+        myBST.deleteNode(2);
+
+        /*
+               3
+              / \
+             1  null
+        */
+
+
+        System.out.println( "\nRoot: " + myBST.getRoot().value );
+        System.out.println( "Root->Left: " + myBST.getRoot().left.value );
+        System.out.println( "Root->Right: " + myBST.getRoot().right );
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+			Root: 2
+			Root->Left: 1
+			Root->Right: 3
+
+			Root: 3
+			Root->Left: 1
+			Root->Right: null
+
+        */
 
 
     }
